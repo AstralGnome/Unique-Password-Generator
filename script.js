595257b562e03a612ea82c3f41fcd9a1bf3daf1b
Astral Gnome <reedkeenan@gmail.com>
2020-10-25 14:31:10 -0700

    Added some initial variables and comments. beginning to add 'if' statements.

--- script.js
+++ script.js
@@ -1,8 +1,64 @@
-// Assignment Code
+//Generate a button that will generate a password.
 var generateBtn = document.querySelector("#generate");
 
+//List all possible password characters in appropriate arrays and define a database to hold them.
+
+var allCharacters = {
+
+  numbers: [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ],
+  lowerCase: [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ],
+  upperCase: [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ],
+  specialChar: [ "\"", " ", "!", "#", "$", "%", "&", "'", "(",")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "/", "]", "^", "_", "`", "{", "|", "}", "~" ]
+}
+
+  //Create the primary function for generating a password.
+  function generatePassword(){
+
+  //Declare a new list of characters to use.
+  var charactersToUse = [];
+
+  // Dictate password length
+  var passwordLength = parseInt(prompt("First, we must define your password length. Choose a number between 8 and 128."));
+
+  if (!(passwordLength >= 8 || passwordLength <= 128)) {
+    alert("Your password must be between 8 and 128 characters.")
+    return;
+  }
+
+    //See if the user wants to use use numbers.
+    var usingNumbers = alert("Would like to use lowercase letters?");
+
+    if (usingNumbers{}
+
+    //See if the user wants to use Uppercase.
+
+    //See if the user wants to use special characters.
+
+
+
+  var password = "";
+
+
+
+// Confirm use number usingNumbers // isUsingLowercaseLetters
+//if using numbers push numbers into the characters to use list
+// confirm use UPPERCASE letters
+// confirm use special characters
+
+
+while (!(password.length < passwordLength)) {
+  //randomly select "randomCharacter" from "charactersToUse"
+  //append "randomCharacter" to "password" string
+}
+
+}
+// return password;??????Why did this head off console.log(numbers)?????
+
+// Assignment Code
+
+
 // Write password to the #password input
-function writePassword() {
+  function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -12,3 +68,10 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// <!--### Instructions
+
+// * Write JavaScript code that does the following:
+// * Using a confirm, ask the user, "Do you like \_?" Store their response in a variable.
+// * Using a prompt, ask the user, "What kind of \_ do you like?" Store their response in a variable.
+// * Alert both variables to the screen.-->
